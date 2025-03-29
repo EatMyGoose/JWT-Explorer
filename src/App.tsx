f26b28997d7ec316b3dd6579ccd846a744bf7c1d
c-w-l <>
2025-03-30 00:38:51 +0800

    css styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,69 +5,83 @@ function App() {
   const jwt = useJWT({
     header: "",
     body: "",
-    secret: "meowing",
+    secret: "super-secret",
     algorithm: "HS256"
   })  
 
   return (
     <>
-      <div className="o-container o-container--xlarge">
-        <h1 className='u-text--highlight'>JWT Editor</h1>
+      <div className='has-background-primary-dark block'>
+        <div className='container pt-3 pb-3'>
+          <h1 className="title is-2">JWT Editor</h1>
+        </div>
       </div>
 
-      <div className="o-container o-container--xlarge">
-        <div className="o-grid">
-          <div className="o-grid__cell c-card">
-            <h3>Token:</h3>
-            <textarea 
-              rows={30}
-              onChange={(e) => jwt.onChangeJwtToken(e.target.value)}
-              value={jwt.encodedJwt}
-              style={jwt.validEncodedJwt? {} : {backgroundColor: "red"}}
-            >
+      <div className="container box columns">
+          <h1></h1>
+          <div className="column">
+            <div className='field'>
+              <label className='label'>JWT Token</label>
+              <textarea 
+                className={`block textarea has-fixed-size ${!jwt.validEncodedJwt? "is-warning" : ""}`}
+                rows={25}
+                onChange={(e) => jwt.onChangeJwtToken(e.target.value)}
+                value={jwt.encodedJwt}
+              >
+              </textarea>
+            </div>
 
-            </textarea>
-            <div>
-              <span>Signature Verified?</span>
-              <input type="checkbox" checked={jwt.signatureVerified}></input>
+            <article className={`message ${jwt.signatureVerified? "is-success": "is-warning"}`}>
+              <div className="message-body">
+                {jwt.signatureVerified? "Signature Verified" : "Invalid Signature"}
+              </div>
+            </article>
+          </div>
+
+          <div className="column">
+            <div className='field'>
+              <label className='label'>Header</label>
+              <textarea 
+                className={`textarea has-fixed-size ${!jwt.jwtHeaderValid? "is-warning" : ""}`}
+                rows={7} 
+                value={jwt.jwtHeader}
+                onChange={(e) => jwt.onChangeHeaders(e.target.value)}
+              />
+            </div>
+
+            <div className='field'>
+              <label className='label'>Body</label>
+              <textarea 
+                className={`textarea has-fixed-size ${!jwt.jwtBodyValid? "is-warning" : ""}`}
+                rows={15} 
+                value={jwt.jwtBody}
+                onChange={(e) => jwt.onChangeBody(e.target.value)}
+              >  
+              </textarea>
+            </div>
+            
+            <div className='field'>
+              <label className='label'>Secret</label>
+              <textarea 
+                className={`textarea has-fixed-size`}
+                rows={3} 
+                value={jwt.jwtSecret}
+                onChange={(e) => jwt.onChangeSecret(e.target.value)}
+              >
+              </textarea>
+            </div>
+
+            <div className='field'>
+              <label className='label'>Algorithm</label>
+              <div className="select is-fullwidth"> 
+                <select value={jwt.algorithm} onChange={e => jwt.onChangeAlgorithm(e.target.value)}>
+                  {
+                    jwt_encryption_algorithms.map(algo => {return <option value={algo}>{algo}</option>})
+                  }
+                </select>
+              </div>
             </div>
           </div>
-          <div className="o-grid__cell c-card">
-            <h3>Decoded:</h3>
-            <h4>Header:</h4>
-            <textarea 
-              rows={5} 
-              value={jwt.jwtHeader}
-              onChange={(e) => jwt.onChangeHeaders(e.target.value)}
-              style={jwt.jwtHeaderValid? {} : {backgroundColor: "red"}}
-            />
-
-            <h4>Body:</h4>
-            <textarea 
-              rows={20} 
-              value={jwt.jwtBody}
-              onChange={(e) => jwt.onChangeBody(e.target.value)}
-              style={jwt.jwtBodyValid? {} : {backgroundColor: "red"}}
-            >  
-            </textarea>
-            
-            <h4>Secret:</h4>
-            <textarea 
-              rows={5} 
-              value={jwt.jwtSecret}
-              onChange={(e) => jwt.onChangeSecret(e.target.value)}
-            >
-              
-            </textarea>
-
-            <h5>Algorithm</h5>
-            <select value={jwt.algorithm} onChange={e => jwt.onChangeAlgorithm(e.target.value)}>
-              {
-                jwt_encryption_algorithms.map(algo => {return <option value={algo}>{algo}</option>})
-              }
-            </select>
-          </div>
-        </div>
       </div>
     </>
   )
